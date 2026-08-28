c75e0875047f7aa1cfcae7d4058bd5703e0c27de
eliokean <yaoeliottkenan@gmail.com>
2026-08-28 03:03:42 +0000

    fix(merchant): add image_picker dependency and import AppColors in merchant settings

--- apps/virelo_marchand/lib/features/profile/presentation/pages/merchant_settings_page.dart
+++ apps/virelo_marchand/lib/features/profile/presentation/pages/merchant_settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lucide_icons/lucide_icons.dart';
+import 'package:virelo_design_system/theme/app_colors.dart';
 import 'package:virelo_design_system/theme/app_text_styles.dart';
 import 'package:virelo_design_system/constants/app_spacing.dart';
 import 'package:virelo_core/network/api_client.dart';
